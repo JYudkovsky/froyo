0c9f79d1d882300d37428d206058073959c31101
josh <josh@joshubuntu.myguest.virtualbox.org>
2023-09-22 10:41:05 -0400

    Added functions

---
+++ froyo.js
@@ -0,0 +1,24 @@
+const userinput = prompt("Please enter your froyo flavors", "vanilla, vanilla, vanilla, strawberry, coffee,coffee")
+
+const carry = userinputer.split(',')
+
+const result = {};
+
+for (const flavor in array) {
+if(result[flavor]){
+    result[flavor] += 1;
+} else {
+    result[flavor] = 1;
+}
+}
+console.table(result);
+
+
+
+
+
+
+
+
+
+
